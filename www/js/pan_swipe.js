f87b68794f0dd5c41e078340150fa8a344d250c2
Joolaa <jola@cs.helsinki.fi>
2015-09-20 23:03:28 +0300

    panswipe add to element method made visible to outside world

--- www/js/pan_swipe.js
+++ www/js/pan_swipe.js
@@ -1,98 +1,96 @@
-function selectPanSwipable(selectors) {
-
-    //function that makes an element pannable and swipable
-    var makeElemPanSwipable = function(el) {
-        var reqAnimationFrame = (function () {
-            return window[Hammer.prefixed(window, 'requestAnimationFrame')]
-                || function (callback) {
-                    window.setTimeout(callback, 1000 / 60);
-                };
-        })();
-        var START_X = 0;
-        var START_Y = 0;
-        var ticking = false;
-        var transform;
-        var timer;
-        var mc = new Hammer.Manager(el, { touchAction: "pan-y" });
-
-        var resetElement = function() {
-            if(!el.classList.contains("animation")) {
-                el.classList.add("animation");
-            }
-            transform = {
-                translate: { x: START_X, y: START_Y },
-                scale: 1,
-                angle: 0,
-                rx: 0,
-                ry: 0,
-                rz: 0
+function makeElemPanSwipable(el) {
+    var reqAnimationFrame = (function () {
+        return window[Hammer.prefixed(window, 'requestAnimationFrame')]
+            || function (callback) {
+                window.setTimeout(callback, 1000 / 60);
             };
-            requestElementUpdate();
+    })();
+    var START_X = 0;
+    var START_Y = 0;
+    var ticking = false;
+    var transform;
+    var timer;
+    var mc = new Hammer.Manager(el, { touchAction: "pan-y" });
+
+    var resetElement = function() {
+        if(!el.classList.contains("animation")) {
+            el.classList.add("animation");
         }
-
-        var updateElementTransform = function() {
-
-            var value = [
-                'translate3d(' + transform.translate.x + 'px, ' + transform.translate.y + 'px, 0)',
-                'scale(' + transform.scale + ', ' + transform.scale + ')',
-                'rotate3d('+ transform.rx +','+ transform.ry +','+ transform.rz +','+  transform.angle + 'deg)'
-            ];
-            value = value.join(" ");
-            el.style.webkitTransform = value;
-            el.style.mozTransform = value;
-            el.style.transform = value;
-            ticking = false;
-        }
-        var requestElementUpdate = function() {
-            if(!ticking) {
-                reqAnimationFrame(updateElementTransform);
-                ticking = true;
-            }
-        }
-        var logEvent = function(str) {
-            //log.insertBefore(document.createTextNode(str +"\n"), log.firstChild);
-        }
-        var onPan = function(ev) {
-            if(el.classList.contains("animation")) {
-                el.classList.remove("animation");
-            }
-            transform.translate = {
-                x: START_X + ev.deltaX,
-                y: START_Y
-            };
-            requestElementUpdate();
-            logEvent(ev.type);
-        }
-
-        var onSwipe = function(ev) {
-            transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
-
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                resetElement();
-            }, 300);
-            requestElementUpdate();
-            logEvent(ev.type);
-            el.style.display='none';
-        }
-
-
-        mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
-        mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
-        mc.on("panstart panmove", onPan);
-        mc.on("swipe", onSwipe);
-
-        mc.on("hammer.input", function(ev) {
-            if(ev.isFinal) {
-                resetElement();
-            }
-        });
-        resetElement();
+        transform = {
+            translate: { x: START_X, y: START_Y },
+            scale: 1,
+            angle: 0,
+            rx: 0,
+            ry: 0,
+            rz: 0
+        };
+        requestElementUpdate();
     }
 
-    //select all elements matching selectors and
-    //apply pannability and swipability
+    var updateElementTransform = function() {
 
+        var value = [
+            'translate3d(' + transform.translate.x + 'px, ' + transform.translate.y + 'px, 0)',
+            'scale(' + transform.scale + ', ' + transform.scale + ')',
+            'rotate3d('+ transform.rx +','+ transform.ry +','+ transform.rz +','+  transform.angle + 'deg)'
+        ];
+        value = value.join(" ");
+        el.style.webkitTransform = value;
+        el.style.mozTransform = value;
+        el.style.transform = value;
+        ticking = false;
+    }
+    var requestElementUpdate = function() {
+        if(!ticking) {
+            reqAnimationFrame(updateElementTransform);
+            ticking = true;
+        }
+    }
+    var logEvent = function(str) {
+        //log.insertBefore(document.createTextNode(str +"\n"), log.firstChild);
+    }
+    var onPan = function(ev) {
+        if(el.classList.contains("animation")) {
+            el.classList.remove("animation");
+        }
+        transform.translate = {
+            x: START_X + ev.deltaX,
+            y: START_Y
+        };
+        requestElementUpdate();
+        logEvent(ev.type);
+    }
+
+    var onSwipe = function(ev) {
+        transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
+
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            resetElement();
+        }, 300);
+        requestElementUpdate();
+        logEvent(ev.type);
+        el.style.display='none';
+    }
+
+
+    mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
+    mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
+    mc.on("panstart panmove", onPan);
+    mc.on("swipe", onSwipe);
+
+    mc.on("hammer.input", function(ev) {
+        if(ev.isFinal) {
+            resetElement();
+        }
+    });
+    resetElement();
+}
+
+//select all elements matching selectors and
+//apply pannability and swipability
+
+function selectPanSwipable(selectors) {
     var elems = document.querySelectorAll(selectors);
 
     if(elems) {
