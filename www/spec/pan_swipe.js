14aade212a76dc6359f6566d8d7627b6ccf53b6c
Anna Salo <anna.emilia.salo@gmail.com>
2015-09-21 02:47:11 +0300

    pan_swipe.js

--- www/spec/pan_swipe.js
+++ www/spec/pan_swipe.js
@@ -1,19 +1,11 @@
 describe("selectPanSwipable", function() {
-	descripe("MakeElemPanSwipable", function() {
-		it("calls the requestElementUpdate() function", function() {
-			var fakeMakeElemPanSwipable = new fakeMakeElemPanSwipable();
-			spyOn(fakeMakeElemPanSwipable, "requestElementUpdate()");
-			fakeMakeElemPanSwipable.resetElement();
-			expect(fakeMakeElementPanSwipable.requestElementUpdate).toHaveBeenCalled();
-		});
+	var elem = document.createElement("div"); 
+	
+	it("should make an element pannable and swipable", function() {
+		makeElemPanSwipable(elem);
+		expect(elem.classList.contains("animation")).toBe(true);
 	});
-				//	descripe("resetElement", function() {
-			
-	//	});
-                	
-        //	});
-			
-			
-		});
+});
+	
 		
 	
