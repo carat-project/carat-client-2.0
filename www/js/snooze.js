d3dac873944a24b806112e0fc1ab2ba4dbd5529b
Anssi Kienanen <anssi.kienanen@cs.helsinki.fi>
2015-09-25 21:02:38 +0300

    function to show hidden cards

--- www/js/snooze.js
+++ www/js/snooze.js
@@ -12,3 +12,20 @@ function cancel(id){
     el.style.display = 'inherit';
 }
 
+//function to show all snoozed cards again and remove from localstorage
+function cancelAll(){
+    
+    var li = document.querySelector("#showAllHiddenCards");
+    
+    li.addEventListener('click', function () {
+        var keys = Object.keys(localStorage);
+        i = keys.length;
+        
+        while (i--) {
+            if (localStorage.getItem(keys[i]) == "dismissed") {
+                cancel(keys[i]);
+                localStorage.removeItem(keys[i]);
+            }
+        }
+    });
+}                        
