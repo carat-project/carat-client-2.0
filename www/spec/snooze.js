a1a1050ac6ea1e6f8739946bf6e0c7201ec1960a
Anna Salo <anna.emilia.salo@gmail.com>
2015-09-26 03:29:02 +0300

    test node added

--- www/spec/snooze.js
+++ www/spec/snooze.js
@@ -1,24 +1,37 @@
-describe("snooze", function() {
+describe("user swipes card to left", function() {
+	var div1 = document.createElement("DIV");
+	div1.setAttribute("class", "mdl-card mdl-shadow--2dp");
+	div1.id = "bluetooth";
 	
-	var id = "bluetooth"; 
+ 	describe("snooze", function() {
 	
-	it("should dismiss card forever", function() {
+		it("should dismiss card forever", function() {
 		toggleVisibility();
-		snooze(id);
-		expect(localStorage.getItem(id).not.toBeNull);
-		expect(localStorage.getItem(id).toBeDefined());
+		snooze(div1.id);
+		expect(localStorage.getItem(div1.id).not.toBeNull);
+		expect(localStorage.getItem(div1.id).toBeDefined());
 		
+		});
 	});
-});
 
-describe("cancel", function() {
+	describe("cancel", function() {
+		
+		var el = document.getElementById(div1.id);
 	
-	var id = "bluetooth";
-	var el = document.getElementById(id);
-	
-	it("should cancel dismissing card forever", function() {
-		cancel(id);
-		expect(localStorage.getItem(id).toBeNull);
-		expect(el.style.display.toBe("inherit"));
+		it("should cancel dismissing card forever", function() {
+			cancel(div1.id);
+			expect(localStorage.getItem(div1.id).toBeNull);
+			expect(el.style.display.toBe("inherit"));
+		});
 	});
+
+	describe("cancel all", function() {
+	
+	
+		it("should show all snoozed cards again and remove from localstorage", function() {
+			cancelAll()
+			expect(localStorage.length().toEqual(0));
+		});
+	});
+
 });
