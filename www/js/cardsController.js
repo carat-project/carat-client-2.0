c24b09195183e0fe30967698862cfba6535922e2
Anna Salo <anna.emilia.salo@gmail.com>
2015-10-09 19:49:48 +0300

    cardsControlleria siistitty clean code periaatteiden mukaiseksi

--- www/js/cardsController.js
+++ www/js/cardsController.js
@@ -13,9 +13,18 @@ itemCards = (function(notificationsArray, panSwipeCallback) {
     //get template Dom-node for a card
     var getNewItemDomNodeTemplate = function() {
 
-        var htmlString = '<div class="mdl-card"><div class="carat-card__title"><div class="mdl-card__icon"></div><div class="mdl-card__title-text"><div class="expand"><i class="material-icons">&#xE5CF;</i></div></div><div class="carat-card-time"></div></div><div class="mdl-card__supporting-text"><div class="collapse"></div></div></div>';
-// commented away<a class="mdl-card__more" role="button">+</a>
-     //   <div class="mdl-card__actions"></div>
+        var htmlString = '<div class="mdl-card">' +
+						 	'<div class="carat-card__title">' +
+						 		'<div class="mdl-card__icon"></div>' +
+						 		'<div class="mdl-card__title-text">' +
+						 			'<div class="expand">' +
+						 				'<i class="material-icons">&#xE5CF;</i></div></div>' +
+						 		'<div class="carat-card-time"></div></div>' +
+						 	'<div class="mdl-card__supporting-text">' +
+							'<div class="collapse"></div></div></div>';
+		    // commented away<a class="mdl-card__more" role="button">+</a>
+     					//   <div class="mdl-card__actions"></div>
+						
         var domNode = parseDomNode(htmlString);
 
         panSwipeCallback(domNode);
@@ -25,7 +34,12 @@ itemCards = (function(notificationsArray, panSwipeCallback) {
 
     //summary card item template
     var getNewSummaryEntryDomNodeTemplate = function() {
-        var htmlString ='<div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone"><div class="carat_summaryCard_app_icon"><div class="mdl-card__icon"></div><i class="material-icons"></i></div><div class="carat_summaryCard_app_name"></div><div class="carat_summaryCard_app_time"></div></div>';
+        var htmlString ='<div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">' +
+							'<div class="carat_summaryCard_app_icon">' + 
+								'<div class="mdl-card__icon"></div>' +
+								'<i class="material-icons"></i></div>' +
+							'<div class="carat_summaryCard_app_name"></div>' + 
+							'<div class="carat_summaryCard_app_time"></div></div>';
 
         var domNode = parseDomNode(htmlString);
 
@@ -34,15 +48,29 @@ itemCards = (function(notificationsArray, panSwipeCallback) {
 
     //summary card template, still partly static
     var getNewSummaryDomNodeTemplate = function() {
-        var htmlString ='<div class="mdl-card mdl-shadow--2dp"><div class="carat-card__title" id="summary"><div class="mdl-card__title-text"></div></div><div class="mdl-card__supporting-text"><div class="carat_summaryCard_group_title" id ="bugTitleAndCount"></div><div id="bugSummaryGrid" class="carat_hide"><div class="mdl-grid carat_summary_grid" id="bugsGrid"></div></div><div class="carat_summaryCard_group_title" id ="hogTitleAndCount"></div><div id="hogSummaryGrid" class="carat_hide"><div class="mdl-grid carat_summary_grid" id="hogsGrid"></div></div><div class="carat_summaryCard_group_title">0 System notifications</div></div><div class="mdl-card__actions"><a class="mdl-card__more" id="summary-button" role="button" onclick="showOrHideActions()" href="#">More</a></div></div>';
+        var htmlString ='<div class="mdl-card mdl-shadow--2dp">' +
+							'<div class="carat-card__title" id="summary">' +
+								'<div class="mdl-card__title-text"></div></div>' +
+							'<div class="mdl-card__supporting-text">' +
+								'<div class="carat_summaryCard_group_title" id ="bugTitleAndCount"></div>' +
+								'<div id="bugSummaryGrid" class="carat_hide">' +
+									'<div class="mdl-grid carat_summary_grid" id="bugsGrid"></div></div>' +
+								'<div class="carat_summaryCard_group_title" id ="hogTitleAndCount"></div>' +
+								'<div id="hogSummaryGrid" class="carat_hide">' +
+									'<div class="mdl-grid carat_summary_grid" id="hogsGrid"></div></div>' +
+								'<div class="carat_summaryCard_group_title">0 System notifications</div></div>' +
+							'<div class="mdl-card__actions">' +
+								'<a class="mdl-card__more" id="summary-button" role="button" onclick="showOrHideActions()" href="#">More</a></div></div>';
 
 
 //left the old template html if we decide to go backwards
 
-//            '<div class="mdl-card mdl-shadow--2dp"><div class="carat_summaryCard_title"><div class="carat_summaryCard_title_text mdl-card__title-text"><i class="material-icons carat_material-icons_arrow">&#xE5CE</i></div><div class="mdl-layout-spacer"></div></div><div class="mdl-card__supporting-text"><div class="mdl-grid"></div></div></div>';
-
-
-
+//            '<div class="mdl-card mdl-shadow--2dp">
+//				<div class="carat_summaryCard_title">
+//					<div class="carat_summaryCard_title_text mdl-card__title-text">
+//						<i class="material-icons carat_material-icons_arrow">&#xE5CE</i></div>
+//					<div class="mdl-layout-spacer"></div></div><div class="mdl-card__supporting-text">
+//						<div class="mdl-grid"></div></div></div>';
 
         var domNode = parseDomNode(htmlString);
 
